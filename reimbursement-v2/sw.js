83f1d6b3cc5ddb6c74ce3524fb0ecbc81043c7cc
tmacbibi <tmacbibi@gmail.com>
2026-09-19 23:54:09 +0800

    chore(reimbursement): bump alpha7 cache

--- reimbursement-v2/sw.js
+++ reimbursement-v2/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reimburse-v2-0-0-alpha-6c';
+const CACHE = 'reimburse-v2-0-0-alpha-7';
 const ASSETS = [
   './', './index.html', './styles.css', './manifest.webmanifest',
   './js/app.js', './js/db.js', './js/rules.js', './js/ui.js', './js/backup.js', './js/pdf.js', './js/drive.js',
