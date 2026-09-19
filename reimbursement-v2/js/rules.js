e9050ab94e50a88edcbb92e08a623558c2681a3f
tmacbibi <tmacbibi@gmail.com>
2026-09-19 23:00:18 +0800

    chore(reimbursement): bump alpha6

--- reimbursement-v2/js/rules.js
+++ reimbursement-v2/js/rules.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '2.0.0-alpha.5';
+export const APP_VERSION = '2.0.0-alpha.6';
 export const MILEAGE_RATE = 8;
 export const MEAL_RATES = { breakfast: 120, lunch: 180, dinner: 180 };
 
